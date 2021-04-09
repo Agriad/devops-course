eedd45180659dbf86694c2fa560130ed2d1b61e2
Justin Arieltan <agriad1@yahoo.com>
2021-04-09 14:14:14 +0200

    Testing bash again 3

--- .github/workflows/read-file.sh
+++ .github/workflows/read-file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 echo "Hello"
-cd /github/workspace
-git checkout khaes-kth-registered_ids
-cat Registered_KTH_IDs.txt
-echo "Done"
+#cd /github/workspace
+#git checkout khaes-kth-registered_ids
+#cat Registered_KTH_IDs.txt
+#echo "Done"
